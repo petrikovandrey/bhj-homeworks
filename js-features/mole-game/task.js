e2bbebbbdddbaf3962cc65e426ea3be1ca83d3b6
Andrey Petrikov <aapetrikov@gmail.com>
2020-02-04 22:09:20 +0200

    task 3.1

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -2,40 +2,37 @@ const array = ["hole1", "hole2", "hole3", "hole4", "hole5", "hole6", "hole7", "h
 const dead = document.getElementById("dead");
 const lost = document.getElementById("lost");
 
+
 for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    document.getElementById("element").onclick = function () {
-        holehasMole = getHoleId();
-        if (holehasMole === click1) {
-            dead.textContent++;
-            if (dead.textContent === 10) {
-                alert("You are the winner!");
-            }
-        } else {
-            lost.textContent++;
-            if (lost.textContent === 5) {
-                alert("You are the loser!");
-            }
-        }
-
-    }
+    let element = array[index];
+    let click = document.getElementById(element);
+    click.onclick = checkClick;
+    console.log(click);
 }
 
-
-function getHoleId() {
-    // const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    for (let index = 0; index < array.length; index++) {
-        const element = array[index];
-        const hole = document.getElementById(element);
-
-        if (hole.className.includes('hole_has-mole')) {
-            return hole;
-        }
-
-    }
-    return undefined;
+function clearStat() {
+    lost.textContent = 0;
+    dead.textContent = 0;
 }
 
-// если ячейка содержит класс тогда +1 иначе +1 к ошибке
-// если ошибок 5 тогда конец игры
-// если 10 побед тогда конец игры
+function getHoleId(element) {
+    return element.className.includes('hole_has-mole');
+}
+
+function checkClick() {
+
+    holehasMole = getHoleId(this);
+    if (holehasMole) {
+        dead.textContent++;
+        if (dead.textContent === "10") {
+            alert("You are the winner!");
+            clearStat();
+        }
+    } else {
+        lost.textContent++;
+        if (lost.textContent === "5") {
+            alert("You are the loser!");
+            clearStat();
+        }
+    }
+}

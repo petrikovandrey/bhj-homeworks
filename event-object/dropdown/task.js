2427f7fb0398ef9a9a082ad2b20484506c57a696
Andrey Petrikov <aapetrikov@gmail.com>
2020-02-18 21:34:14 +0200

    task1.1

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -3,10 +3,10 @@ const list = document.querySelector(".dropdown__list");
 const links = document.getElementsByClassName("dropdown__link");
 for (let index = 0; index < links.length; index++) {
     links[index].onclick = makeChoise;
-
 }
 
 dropdown.onclick = dropdownEvent;
+const listActive = "dropdown__list_active";
 
 function makeChoise(e) {  
     e.preventDefault();
@@ -15,11 +15,9 @@ function makeChoise(e) {
 }
 
 function dropdownEvent() {
-    
-    if (list.className.indexOf("dropdown__list_active") > 0) {
-        list.className = "dropdown__list";
+    if(list.classList.contains(listActive)) {
+       list.classList.remove(listActive) 
     } else {
-        list.className += " dropdown__list_active";
+        list.classList.add(listActive)   
     }
-
 }

f512a9d7991ee65b5af91c17b1d0984c9d6c7cdf
Andrey Petrikov <aapetrikov@gmail.com>
2020-02-29 21:43:30 +0200

    done 2

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,16 +1,25 @@
 let checkBoxes = document.getElementsByClassName("interest__check");
 let interests = document.getElementsByClassName("interests");
 interests = Array.from(interests);
-console.log(checkBoxes);
+// console.log(checkBoxes);
 for (let index = 0; index < checkBoxes.length; index++) {
     checkBoxes[index].onclick = click;
 }
 
 function click() {
-    let interest = this.closest(".interests");
-    let id = interests.find(ithem => ithem === interest);
-    console.log(interest);
-    root = interest.parentElement;
-    console.log(root);
-    root.firstElementChild.checked = !interest.firstElementChild.checked;
+    let interest = this.closest(".interests_active");
+ 
+    let list;
+    if (interest === null) {
+        interest = this.closest("li");
+        list = interest.querySelectorAll(".interest__check");
+    } else {
+        list = interest.parentElement.querySelectorAll(".interest__check");
+    }
+  
+    list.forEach(element => {
+        if (this != element) {
+            element.checked = !element.checked;
+        }
+    });
 }

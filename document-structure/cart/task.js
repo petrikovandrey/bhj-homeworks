db6f9b48892c167cc4e9e07b431132c53d18f6b2
Andrey Petrikov <aapetrikov@gmail.com>
2020-03-05 20:59:20 +0200

    done3

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -3,6 +3,9 @@ let plus = document.getElementsByClassName("product__quantity-control_inc");
 let add = document.getElementsByClassName("product__add");
 let cart = document.querySelector(".cart__products");
 
+let pqc = "product__quantity-controls";
+let pqv = "product__quantity-value";
+
 for (let index = 0; index < minus.length; index++) {
     minus[index].onclick = clickMinus;
 }
@@ -16,10 +19,9 @@ for (let index = 0; index < add.length; index++) {
 }
 
 function clickMinus() {
-    let c = this.closest(".product__quantity-controls");
-    let v = c.querySelector(".product__quantity-value");
+    let c = this.closest("." + pqc);
+    let v = c.querySelector("." + pqv);
     let n = Number(v.textContent);
-
     if (n === 1) {
         return;
     }
@@ -27,8 +29,8 @@ function clickMinus() {
 }
 
 function clickPlus() {
-    let c = this.closest(".product__quantity-controls");
-    let v = c.querySelector(".product__quantity-value");
+    let c = this.closest("." + pqc);
+    let v = c.querySelector("." + pqv);
     let n = Number(v.textContent);
     v.textContent = n + 1;
 }
@@ -40,15 +42,39 @@ function clickAdd(e) {
     let qty = Number(v.textContent);
     let p = this.closest(".product");
     let id = p.dataset.id;
-    let src = p.querySelector(".product__image").src;
-    
-    cart.append(createCart(id,src,qty));
-    // добавить проверку на наличие элемента
-    // менять его кол-вл
-    // добавить анимацию
+    let img = p.querySelector(".product__image");
+    let src = img.src;
+
+    let x = e.pageX;
+    let y = e.pageY;
+    let clone = img.cloneNode(false);
+    clone.setAttribute("style", `border-radius: 49%; position: absolute; left: ${x}px; top: ${y}px;`);
+    p.appendChild(clone);
+
+    let setI = setInterval(() => {
+        x += 1;
+        y -= 10;
+        if (y < 0) {
+            clone.remove();
+            clearInterval(setI);
+        }
+        clone.setAttribute("style", `border-radius: 49%; position: absolute; left: ${x}px; top: ${y}px;`);
+    }, 10, x, y, clone);
+
+    let carts = document.getElementsByClassName("cart__product");
+    carts = Array.from(carts);
+    let itsNew = carts.find(element => element.dataset.id === id);
+    if (itsNew != undefined) {
+        let v = itsNew.querySelector(".cart__product-count");
+        let n = Number(v.textContent);
+        v.textContent = n + qty;
+    } else {
+        cart.append(createCart(id, src, qty));
+    }
 }
 
-function createCart(id,src,qty) {
+
+function createCart(id, src, qty) {
     let product = document.createElement("div");
     let image = document.createElement("img");
     let count = document.createElement("div");
